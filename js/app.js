3d03604cbba63468ae4c3daba1528b32c7d87faa
Arvin Alvea <arvinalvea@gmail.com>
2020-07-03 11:57:28 -0400

    added functions and click handlers to app.js

--- js/app.js
+++ js/app.js
@@ -65,3 +65,20 @@ fetch(urlAPI)
     .then(displayEmployees)
     .catch(err => console.log(err))
 
+// Event Listeners
+gridContainer.addEventListener('click', e => {
+
+    // make sure the click is not on the gridContainer itself
+    if(e.target !== gridContainer) {
+
+        // select the card element based on its proximity to actual element clicked
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+});
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add("hidden");
+});
